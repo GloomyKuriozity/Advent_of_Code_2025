a8f6efd0689cea1108bd532857141a9e41061efc
Kuriozity <gloomykuriozity@gmail.com>
2025-12-02 16:04:11 +0100

    Refactor validation logic and aggregate results
    Refactor is_invalid function to check for repeating blocks and update find_invalid_in_range to return the sum of invalid numbers.

--- #Advent of Code 2#.py
+++ #Advent of Code 2#.py
@@ -4,16 +4,26 @@ with open('/home/robdev/Desktop/Advent of Code/Advent_of_Code_ID_List.txt') as f
     lines = f.read().split(',')
 
 stripped = [p.split(',') for p in lines]
+results = 0
 
 def is_invalid(n):
     s = str(n)
-    if len(s) % 2 != 0:
-        return False
-    half = len(s) // 2
-    return s[:half] == s[half:]
+    L = len(s)
+
+    for k in range(1, L):
+        if L % k == 0:                 
+            block = s[:k]
+            if block * (L // k) == s:     
+                return True
+
+    return False
 
 def find_invalid_in_range(start, end):
-    return [n for n in range(start, end+1) if is_invalid(n)]
+    tab = [n for n in range(start, end+1) if is_invalid(n)]
+    if tab != []:
+        return sum(tab)
+    return 0
+
 
 for element in stripped:
     cleaned = [p.split('-') for p in element]
@@ -21,4 +31,6 @@ for element in stripped:
     start = int(cleaned[0][0])
     end = int(cleaned[0][1])
 
-    print(find_invalid_in_range(start, end))
+    results += find_invalid_in_range(start, end)
+
+print(results)
